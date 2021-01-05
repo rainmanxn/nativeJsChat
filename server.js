d10c9a56aa6a7bfc8aec0007cf7a0d026daa860f
rainmanxn <osryazantsev@gmail.com>
2021-01-05 19:07:25 +0300

    add login page

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require('express');
+const express = require('express')
 
-const app = express();
-const PORT = 4000;
+const app = express()
+const PORT = 4000
 
-app.use(express.static('./static/'));
+app.use(express.static('./static/'))
 
 app.listen(PORT, function () {
-  console.log(`Example app listening on port ${PORT}!`);
-});
+	console.log(`Example app listening on port ${PORT}!`)
+})
