8999403c9e481439c97a7029b247c366e6a27ba3
rainmanxn <osryazantsev@gmail.com>
2021-01-12 20:54:31 +0300

    fix cf review

--- static/src/scripts/login.js
+++ static/src/scripts/login.js
@@ -1,22 +1,15 @@
-let login;
-let password;
-const data = {}
+const data = Array.from(document.querySelectorAll('.input-text'))
+  .reduce((acc, el) => {
+    el && el.addEventListener('input', () => {
+      const { type, value } = el;
+      acc[type] = value;
+    });
+    return acc
+  }, {})
+const submitButton = document.querySelector('form');
 
-const inputUserName = document.getElementById('userName');
-const inputUserPassword = document.getElementById('password');
-const submitButton = document.getElementById('submit');
 
-inputUserName.addEventListener('input', () => {
-  login = inputUserName.value;
-  data.login = login;
-});
-
-inputUserPassword.addEventListener('input', () => {
-  password = inputUserPassword.value;
-  data.password = password;
-})
-
-submitButton.addEventListener('submit', (event) => {
+submitButton && submitButton.addEventListener('submit', (event) => {
   event.preventDefault();
   console.log(data)
 })
--- static/src/scripts/register.js
+++ static/src/scripts/register.js
@@ -1,63 +1,27 @@
-let email;
-let login;
-let first_name;
-let second_name;
-let password;
-let password_confirm;
-let phone;
-const data= {};
+const inputElements = Array.from(document.querySelectorAll('.input-text'))
+const data = inputElements
+  .reduce((acc, el) => {
+    el && el.addEventListener('input', () => {
+      const { name, value } = el;
+      acc[name] = value;
+    });
+    return acc
+  }, {})
 
-const inputEmail = document.getElementById('email');
-const inputUserName = document.getElementById('userName');
-const inputFirstName = document.getElementById('firstName');
-const inputLastName = document.getElementById('lastName');
-const inputPhone = document.getElementById('phone');
-const inputUserPassword = document.getElementById('password');
-const inputUserPasswordConfirm = document.getElementById('passwordConfirm');
 const submitButton = document.getElementById('submit');
 const errorMessage = document.getElementById('errorMessage');
 
-inputEmail.addEventListener('input', () => {
-  email = inputEmail.value;
-  data.email = email;
-});
-
-inputUserName.addEventListener('input', () => {
-  login = inputUserName.value;
-  data.login = login;
-});
-
-inputFirstName.addEventListener('input', () => {
-  first_name = inputFirstName.value;
-  data.first_name = first_name;
-});
-
-inputLastName.addEventListener('input', () => {
-  second_name = inputLastName.value;
-  data.second_name = second_name;
-});
-
-inputPhone.addEventListener('input', () => {
-  phone = inputPhone.value;
-  data.phone = phone;
-});
-
-inputUserPassword.addEventListener('input', () => {
-  password = inputUserPassword.value;
-  data.password = password;
-})
-
-inputUserPasswordConfirm.addEventListener('input', () => {
-  password_confirm = inputUserPasswordConfirm.value;
-  data.password_confirm = password_confirm;
-})
-
-submitButton.addEventListener('submit', (event) => {
+submitButton && submitButton.addEventListener('submit', (event) => {
   event.preventDefault();
+  const { password, password_confirm } = data;
   if (password !== password_confirm) {
-    inputUserPassword.classList.add('incorrect');
-    inputUserPasswordConfirm.classList.add('incorrect');
-    errorMessage.classList.add('error-message__show');
+    inputElements.forEach((element) => {
+      const { name } = element;
+      if (name === 'password' || name === 'password_confirm') {
+        element.classList.add('incorrect');
+      }
+    })
+    errorMessage && errorMessage.classList.add('error-message__show');
   }
   console.log(data)
 })
