f978d34b8f063b9a8da65f26f35ae38149e48391
rainmanxn <osryazantsev@gmail.com>
2021-01-25 20:47:23 +0300

    refactor profile to templator

--- static/src/scripts/createTemplate.js
+++ static/src/scripts/createTemplate.js
@@ -53,15 +53,49 @@ export const createAvatarBlockTemplate = data => {
   return compile(template)(data)
 }
 
-`<div class="profile-avatar-block" id="profileAvatarBlock">
-  <div class="profile-avatar-line"></div>
-  <div class="profile-avatar">
-    <div class="profile-avatar-hover">
-      <div class="profile-avatar-text">Поменять аватар</div>
-    </div>
-    <img src="../img/icon-man.svg" class="profile-avatar-icon" alt="profile-avatar-icon">
-  </div>
-</div>`
+export const createProfileInfoTemplate = data => {
+  const template = `
+    <form id="submit" class="profile-info-block">
+      {{#each fields}}
+        <div class="profile-info-item">
+          <span class="profile-info-field-name">{{fieldName}}</span>
+          <label>
+            <input type="{{inputType}}" readonly class="profile-info-field-input" name="{{inputName}}" value="{{inputValue}}">
+          </label>
+        </div>
+      {{/each}}
+      <div class="profile-info-block">
+        <div id="editBlock">
+          {{#each buttons}}
+            <div class="profile-info-item">
+              <button type="button" id="{{buttonId}}" class="profile-edit-item">{{buttonName}}</button>
+            </div>            
+          {{/each}}
+        </div>
+        <button type="submit" id="saveButton" class="submit-button profile-info-block__submit-button hide-field">СОХРАНИТЬ</button>
+      </div>
+    </form>
+  `
+  return compile(template)(data)
+}
+
+export const createChangePasswordTemplate = data => {
+  const template = `
+    <form id="editPassword" class="profile-info-block profile-info-block__edit-password  hide-field">
+      {{#each fields}}
+        <div class="profile-info-item">
+          <div class="profile-info-field-name">{{fieldName}}</div>
+          <label>
+            <input type="{{inputType}}" class="profile-info-field-input" name="{{inputName}}" value="{{inputValue}}">
+          </label>
+        </div>
+      {{/each}}
+      <button type="submit" id="savePasswordButton" class="submit-button submit-button__edit-password">СОХРАНИТЬ</button>
+    </form>
+  `
+  return compile(template)(data)
+}
+
 
 // `<div class="modal remove-modal" id="modalAddUser">
 //   <div class="modal-header">Добавить пользователя</div>
--- static/src/scripts/profile.js
+++ static/src/scripts/profile.js
@@ -1,4 +1,4 @@
-import { createModalAvatarTemplate, createAvatarBlockTemplate } from './createTemplate.js'
+import { createModalAvatarTemplate, createAvatarBlockTemplate, createProfileInfoTemplate, createChangePasswordTemplate } from './createTemplate.js'
 
 const profileBlock = document.querySelector('.profile-block');
 const avatarModal = document.getElementById('avatarModal');
@@ -9,9 +9,86 @@ const avatarData = {
   srcImg: '../img/icon-man.svg',
   userName: 'Иван'
 }
-const compiledAvatarBlockTemplate = createAvatarBlockTemplate(avatarData)
-profileBlock.innerHTML = compiledAvatarBlockTemplate;
-console.log(compiledAvatarBlockTemplate)
+const profileInfoData = {
+  fields: [
+    {
+      fieldName: 'Почта',
+      inputType: 'email',
+      inputName: 'email',
+      inputValue: 'pochta@yandex.ru'
+    },
+    {
+      fieldName: 'Логин',
+      inputType: 'text',
+      inputName: 'login',
+      inputValue: 'ivanivanov'
+    },
+    {
+      fieldName: 'Имя',
+      inputType: 'text',
+      inputName: 'firstName',
+      inputValue: 'Иван'
+    },
+    {
+      fieldName: 'Фамилия',
+      inputType: 'text',
+      inputName: 'lastName',
+      inputValue: 'Иванов'
+    },
+    {
+      fieldName: 'Имя в чате',
+      inputType: 'text',
+      inputName: 'displayName',
+      inputValue: 'Иваныч'
+    },
+    {
+      fieldName: 'Телефон',
+      inputType: 'tel',
+      inputName: 'phone',
+      inputValue: '+7 (909) 967 30 30'
+    }],
+  buttons: [
+    {
+      buttonId: 'editButton',
+      buttonName: 'Изменить данные'
+    },
+    {
+      buttonId: 'editPasswordButton',
+      buttonName: 'Изменить пароль'
+    },
+    {
+      buttonId: 'exitButton',
+      buttonName: 'Выйти'
+    },
+  ]
+};
+const profilePasswordData = {
+  fields: [
+    {
+      fieldName: 'Старый пароль',
+      inputType: 'password',
+      inputName: 'oldPassword',
+      inputValue: '1111'
+    },
+    {
+      fieldName: 'Новый пароль',
+      inputType: 'password',
+      inputName: 'newPassword',
+      inputValue: '22222'
+    },
+    {
+      fieldName: 'Повторите новый пароль<',
+      inputType: 'password',
+      inputName: 'newPasswordConfirm',
+      inputValue: '22222'
+    },
+  ]
+}
+const compiledAvatarBlockTemplate = createAvatarBlockTemplate(avatarData);
+const compiledProfileInfoTemplate = createProfileInfoTemplate(profileInfoData);
+const compiledChangePasswordTemplate = createChangePasswordTemplate(profilePasswordData);
+
+profileBlock.innerHTML = compiledAvatarBlockTemplate + compiledProfileInfoTemplate + compiledChangePasswordTemplate;
 
 const submitForm = document.getElementById('submit');
 const editButton = document.getElementById('editButton');
